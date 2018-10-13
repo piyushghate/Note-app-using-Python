92907e5f29637285c955093a1478895461b8ec63
piyushghate <piyushghate17@gmail.com>
2018-10-14 02:33:46 +0530

    add delete fucntion and also a bit change in how to save the data.json file

--- notes.py
+++ notes.py
@@ -1,5 +1,6 @@
 import json
 
+# Fetch all the Notes stored on data.json file
 def fetchNote():
     try:
         openFile = open('data.json', 'r');
@@ -7,6 +8,12 @@ def fetchNote():
     except:
         return [];
 
+# function to save the notes in data.json file
+def saveNotes(parseFile):
+    with open('data.json', 'w') as testfile:
+        json.dump(parseFile, testfile);    
+
+# Adding a Note to data.json file
 def addNote():
     print ('Adding a Note');
     parseFile = fetchNote();
@@ -18,6 +25,7 @@ def addNote():
             'title' : title1, 
             'body': body1
          };
+         
     def duplicateFunc():
         for each in parseFile:
             if (each['title'] == data.get('title')):
@@ -30,19 +38,33 @@ def addNote():
         print('Note not added!!');
     else:    
         parseFile.append(data);
-        with open('data.json', 'w') as testfile:
-            json.dump(parseFile, testfile);
+        saveNotes(parseFile);
         print('Note with title: '+data.get('title')+ ' added!');
 
+# Listing all the Notes from data.json file
 def listNote():
     parseFile = fetchNote();
     for each in parseFile:
         print('Title: '+ each['title']+ ' Body: '+each['body']);
 
+# Remove a note from data.json file
 def removeNote():
     print('Removing a Note');
-    
+    parseFile = fetchNote();
+    titleRemove = input('What you want to delete? ');
+    for each in parseFile:
+        x = False;
+        if(each['title'] == titleRemove):
+            parseFile.remove(each);
+            x = True;
+            break;
+    if (x == True):
+        saveNotes(parseFile);
+        print('Note with title: '+titleRemove+ ' removed!');
+    else:
+        print('Nothing found');
 
+# Read a note from data.json file
 def readNote():
     print('Reading a Note');
     parseFile = fetchNote();
--- playground.py
+++ playground.py
@@ -52,29 +52,70 @@ import json
 # with open('test.json', 'w') as testfile:
 #     json.dump(parseFile, testfile);
 
-data = {
-            'title' : 'pulkit', 
-            'body' : 'body1',
-         };
+# data = {
+#             'title' : 'pulkit', 
+#             'body' : 'body1',
+#          };
 
-# print(data.get('title'));
+# # print(data.get('title'));
 
-###################################filter#################################
-try:
-    openFile = open('test.json', 'r');
-    print(openFile);
-    parseFile = json.load(openFile);
-    print(parseFile);
-except:
-    parseFile = [];
+# ###################################filter#################################
+# try:
+#     openFile = open('test.json', 'r');
+#     print(openFile);
+#     parseFile = json.load(openFile);
+#     print(parseFile);
+# except:
+#     parseFile = [];
 
-list1 = {'b':2,2:'a',3:4,'c':5};
+# list1 = {'b':2,2:'a',3:4,'c':5};
 
-# for each in parseFile:                            #### To LIST
-#     print('Title available are: '+ each['title']+ ' with body as: '+each['body']);
+# # for each in parseFile:                            #### To LIST
+# #     print('Title available are: '+ each['title']+ ' with body as: '+each['body']);
 
-for each in parseFile:
-    if (each['title'] == data.get('title')):
-        return True;
-    else:
-        return False;
+# for each in parseFile:
+#     if (each['title'] == data.get('title')):
+#         return True;
+#     else:
+#         return False;
+
+
+
+
+# list1 = [{"title": "piyush", "body": "ghate"}, {"title": "vikas", "body": "c"}, {"title": "prashant", "body": "ohol"}]
+
+# userInput = input('What you want to delete? ');
+
+
+# for each in list1:
+#     x = False;
+#     if(each['title'] == userInput):
+#         print(each);
+#         list1.remove(each);
+#         x = True;
+#         break;
+
+# if (x == True):
+#     pass;
+# else:
+#     print('Nothing found');
+
+# print (list1);
+
+
+# print('Reading a Note');
+#     parseFile = fetchNote();
+#     userInput = input('What you want to delete? ');
+#     for each in parseFile:
+#         x = False;
+#         if(each['title'] == userInput):
+#             print('Found the note with title: '+userInput+ ' as ' +each);
+#             parseFile.remove(each);
+#             x = True;
+#             break;
+#     if (x == True):
+#         with open('data.json', 'w') as testfile:
+#             json.dump(parseFile, testfile);
+#             print('Note with title: '+userInput+ ' removed!');
+#     else:
+#         print('Nothing found');
